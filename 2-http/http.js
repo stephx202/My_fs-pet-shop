756065fd958051387392c8d47252ecb50f994902
stephx202 <stephhernandez202@gmail.com>
2023-12-06 14:38:34 -0800

    all data of pets prints /pets

---
+++ 2-http/http.js
@@ -0,0 +1,33 @@
+let http = require('http');
+let port = process.env.PORT || 8000;
+
+let fs = require('fs');
+
+
+//create the server
+let server = http.createServer(function(req, res){
+    //if the request method GET && request url /pets
+    if(req.method === 'GET' && req.url === '/pets'){
+        fs.readFile("../pets.json", "utf-8", function (error, data) {
+            if(error){
+                res.statusCode = 500;
+                res.end()
+                return;
+            }
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json' );
+            console.log('someth')
+            res.end(data);
+        })
+    } else{
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Not found');
+    }
+})
+//this just shows that it started the server
+server.listen(port, function() {
+  console.log('Listening on port', port);
+});
+
+
