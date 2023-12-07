26bccce75be41dd8e14d6df39adaff2caf9ba79e
stephx202 <stephhernandez202@gmail.com>
2023-12-07 15:52:55 -0800

    completed

--- 3-express/express.js
+++ 3-express/express.js
@@ -5,10 +5,6 @@ const fs = require('fs');
 // import express from 'express';
 // import fs from 'fs';
 // const app = express();
-// make a variable of data array
-// let dataArray  
-// const index = Number(req.url.slice(req.url.lastIndexOf('/') + 1))  => localhost:800/pets/0
-//readfiile in the pets.json
 fs.readFile('../pets.json', 'utf-8', (error, data)  => {
     if(error){
         console.error("error, cant read pet data")
@@ -24,7 +20,7 @@ fs.readFile('../pets.json', 'utf-8', (error, data)  => {
     
     // set up the routes (endpoints)
     app.get('/pets', function (req, res) { // this is handling an if statement for method and url, somehow...
-        res.send( dataparsed /* some path?? here */);
+        res.send( dataparsed);
         console.log("it works")
     });
 
@@ -47,77 +43,10 @@ fs.readFile('../pets.json', 'utf-8', (error, data)  => {
        res.status(err.status).json({ error: err })
        
     });
-    
+  
 });
 
 // start the server listening
 app.listen(8000, function() {
     console.log('server listening on port 8000');
 });
-
-
-
-
-
-
-// app.get('/:num', (req, res, next) => {
-//     const num = req.params.num
-//     if (!Number(num)) {
-//       next({ status: 400, message: 'Please enter a number!' })
-//     } else {
-//       res.json({ message: `${num} is a great number.` })
-           //let petsNum = (`/pets${num}`)
-          // res.send(dataparsed[num])
-//     }
-//   })
-  
-//   app.use((err, req, res, next) => {
-//     res.status(err.status).json({ error: err })
-//   })
-
-
-
-// //set up dependencies
-// let express = require('express');
-// let fs = require('fs')
-// let app = express()
-// let port = process.env.PORT || 8000;
-
-
-// //handle requests with routes. call in 
-// app.get('/pets', (req, res) =>{
-//     res.status(200)
-//     res.send(petsData())
-// })
-
-
-
-// //need to readfile to get data, maybe wrap in a function and invoke in "handle requests with routes"?
-// function petsData(){
-//     fs.readFile('../pets.json', 'utf-8', (error, data) => {
-//         if(error){
-//             console.erorr('not working on receiving data')
-//         }else{
-//             //res.status(200);
-//             //res.set('Content-Type', 'application/json' );
-//             console.log('it is working')
-//             console.log(data);
-//         }
-//     })
-// }
-
-
-// //listen on a port
-// app.listen(port, function(){
-//     console.log("server is running", port)
-// })
-function readPetsData(callback) {
-    fs.readFile('pets.json', 'utf-8', function (err, data) {
-      if (err) {
-        console.error('Error reading pets data:', err.message);
-        callback([]);
-      } else {
-        callback(JSON.parse(data));
-      }
-    });
-  }
