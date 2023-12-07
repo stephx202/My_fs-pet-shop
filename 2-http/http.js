89290c12816ebe2ffcb649e1bfec93f4f0ebecf3
stephx202 <stephhernandez202@gmail.com>
2023-12-07 08:48:46 -0800

    pets data

--- 2-http/http.js
+++ 2-http/http.js
@@ -6,12 +6,14 @@ let fs = require('fs');
 
 //create the server
 let server = http.createServer(function(req, res){
+    
     //if the request method GET && request url /pets
     if(req.method === 'GET' && req.url === '/pets'){
         fs.readFile("../pets.json", "utf-8", function (error, data) {
             if(error){
                 res.statusCode = 500;
                 res.end()
+                //callback(error)---?
                 return;
             }
             res.statusCode = 200;
@@ -19,11 +21,14 @@ let server = http.createServer(function(req, res){
             console.log('someth')
             res.end(data);
         })
-    } else{
+    
+        
+    }else{
         res.statusCode = 404;
         res.setHeader('Content-Type', 'text/plain');
         res.end('Not found');
     }
+    
 })
 //this just shows that it started the server
 server.listen(port, function() {
